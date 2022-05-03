5163e902077d92747753193c94ae6e35940acbae
13stiset <99735833+13stiset@users.noreply.github.com>
2022-05-03 13:51:30 +0200

    Oppdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    spaceship.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    spaceship.move(1)
+})
+let spaceship: game.LedSprite = null
+spaceship = game.createSprite(2, 2)
+let enemy = game.createSprite(0, 2)
 basic.forever(function () {
-	
+    if (spaceship.isTouching(enemy)) {
+        enemy.delete()
+    }
+    if (enemy.isDeleted()) {
+        game.gameOver()
+    }
 })
